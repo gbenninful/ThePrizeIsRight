7add4e9a8887b07a1a2e5e8f15aad822fa1b8d71
gbenninful <enninful@hotmail.com>
2014-02-06 21:21:39 -0800

    Avoiding pollution of Global Namespace

--- PrizeIsRight.js
+++ PrizeIsRight.js
@@ -1,61 +1,69 @@
-﻿var prizes = document.getElementsByName("prize");
-var showPrize = document.getElementsByName("display");
+﻿"use strict";
 
-function reward() {
+//Program doesn't run when I try to avoid polluting Global Namespace
+(function () {
 
-    for (i = 0; i < prizes.length; i++) {
+    var prizes = document.getElementsByName("prize");
+    var showPrize = document.getElementsByName("display");
 
-        if (prizes[i].checked) {
-            var selectedPrize = prizes[i].value;
+    function reward() {
 
-            if (selectedPrize === "door 1") {
+        for (var i = 0; i < prizes.length; i++) {
 
-                showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
+            if (prizes[i].checked) {
+                var selectedPrize = prizes[i].value;
 
-            } else if (selectedPrize === "door 2") {
+                if (selectedPrize === "door 1") {
 
-                showPrize[0].value = "You won a CAT";
+                    showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
 
-            } else if (selectedPrize === "door 3") {
+                } else if (selectedPrize === "door 2") {
 
-                showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
+                    showPrize[0].value = "You won a CAT";
 
-            } else if (selectedPrize === "door 4") {
+                } else if (selectedPrize === "door 3") {
 
-                showPrize[0].value = "You won a BOAT";
+                    showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
 
+                } else if (selectedPrize === "door 4") {
+
+                    showPrize[0].value = "You won a BOAT";
+
+                } else {
+
+                    showPrize[0].value = "Sorry I did not understand your selection";
+                }
             } else {
-
-                showPrize[0].value = "Sorry I did not understand your selection";
+                showPrize[0].innerHTML = "Please make a selection and click submit";
             }
-        } else {
-            showPrize[0].innerHTML = "Please make a selection and click submit";
+
         }
 
     }
 
-}
 
+    function reset() {
 
-function reset() {
+        for (var i = 0; i < prizes.length; i++) {
 
-    for (var i = 0; i < prizes.length; i++) {
-
-        if (prizes[i].checked) {
-            prizes[i].checked = false;
-        } 
-    }
-
-    for (i = 0; i < showPrize.length; i++) {
-
-        if (showPrize[i].value) {
-            showPrize[i].value = "";
-        } else {
-            showPrize[0].innerHTML = "Please make a selection and click submit";
+            if (prizes[i].checked) {
+                prizes[i].checked = false;
+            }
         }
+
+        for (i = 0; i < showPrize.length; i++) {
+
+            if (showPrize[i].value) {
+                showPrize[i].value = "";
+            } else {
+                showPrize[0].innerHTML = "Please make a selection and click submit";
+            }
+        }
+
     }
 
-}
+    //Note: If reset or save btn is clicked no input msg shows("Please make a selection and click submit") in textarea. 
+    //Above msg shows if browser is refreshed and same action is carried out
 
-//Note: If reset or save btn is clicked no input msg shows("Please make a selection and click submit") in textarea. 
-//Above msg shows if browser is refreshed and same action is carried out
+})();
+
