ec9fe68127cb2dd1eda58f2530b6ed5ff74cc70f
gbenninful <enninful@hotmail.com>
2014-02-08 17:01:21 -0800

    Debugging- Messing with the click event
    Try to resolve the issue of my click event  being undefined

--- PrizeIsRight.js
+++ PrizeIsRight.js
@@ -3,67 +3,73 @@
 //Program doesn't run when I try to avoid polluting Global Namespace
 (function () {
 
-    var prizes = document.getElementsByName("prize");
-    var showPrize = document.getElementsByName("display");
+    window.onload = function () {
 
-    function reward() {
 
-        for (var i = 0; i < prizes.length; i++) {
 
-            if (prizes[i].checked) {
-                var selectedPrize = prizes[i].value;
+        var prizes = document.getElementsByName("prize");
+        var showPrize = document.getElementsByName("display");
 
-                if (selectedPrize === "door 1") {
+        prizes.onclick = reward;
 
-                    showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
+        var reward = (function () {
 
-                } else if (selectedPrize === "door 2") {
+            for (var i = 0; i < prizes.length; i++) {
 
-                    showPrize[0].value = "You won a CAT";
+                if (prizes[i].checked) {
+                    var selectedPrize = prizes[i].value;
 
-                } else if (selectedPrize === "door 3") {
+                    if (selectedPrize === "door 1") {
 
-                    showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
+                        showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
 
-                } else if (selectedPrize === "door 4") {
+                    } else if (selectedPrize === "door 2") {
 
-                    showPrize[0].value = "You won a BOAT";
+                        showPrize[0].value = "You won a CAT";
 
+                    } else if (selectedPrize === "door 3") {
+
+                        showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
+
+                    } else if (selectedPrize === "door 4") {
+
+                        showPrize[0].value = "You won a BOAT";
+
+                    } else {
+
+                        showPrize[0].value = "Sorry I did not understand your selection";
+                    }
                 } else {
-
-                    showPrize[0].value = "Sorry I did not understand your selection";
+                    showPrize[0].innerHTML = "Please make a selection and click submit";
                 }
-            } else {
-                showPrize[0].innerHTML = "Please make a selection and click submit";
+
+            }
+
+        })();
+
+
+        function reset() {
+
+            for (var i = 0; i < prizes.length; i++) {
+
+                if (prizes[i].checked) {
+                    prizes[i].checked = false;
+                }
+            }
+
+            for (i = 0; i < showPrize.length; i++) {
+
+                if (showPrize[i].value) {
+                    showPrize[i].value = "";
+                } else {
+                    showPrize[0].innerHTML = "Please make a selection and click submit";
+                }
             }
 
         }
 
-    }
-
-
-    function reset() {
-
-        for (var i = 0; i < prizes.length; i++) {
-
-            if (prizes[i].checked) {
-                prizes[i].checked = false;
-            }
-        }
-
-        for (i = 0; i < showPrize.length; i++) {
-
-            if (showPrize[i].value) {
-                showPrize[i].value = "";
-            } else {
-                showPrize[0].innerHTML = "Please make a selection and click submit";
-            }
-        }
-
-    }
-
-    //Note: If reset or save btn is clicked no input msg shows("Please make a selection and click submit") in textarea. 
-    //Above msg shows if browser is refreshed and same action is carried out
-
+        //Note: If reset or save btn is clicked no input msg shows("Please make a selection and click submit") in textarea. 
+        //Above msg shows if browser is refreshed and same action is carried out
+    };
 })();
 
