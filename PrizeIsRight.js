5256b6d6f90c1aa35e12daf731cb53b2185e2c54
gbenninful <enninful@hotmail.com>
2014-02-18 09:13:17 -0800

    Running code through Js Lint

--- PrizeIsRight.js
+++ PrizeIsRight.js
@@ -2,67 +2,66 @@
 
     "use strict";
 
-    var prizes = document.getElementsByName("prize");
-    var showPrize = document.getElementsByName("display");
+    var prizes = document.getElementsByName("prize"),
+        showPrize = document.getElementsByName("display"),
+        i,
 
-    var reward = function () {
+        reward = function () {
+            var selectedPrize = prizes[i].value;
 
-        document.getElementById("submitBtn").onclick = function () {
+            document.getElementById("submitBtn").onclick = function () {
 
-            for (var i = 0; i < prizes.length; i++) {
+                for (i = 0; i < prizes.length; i = i + 1) {
 
-                if (prizes[i].checked) {
-                    var selectedPrize = prizes[i].value;
+                    if (prizes[i].checked) {
 
-                    if (selectedPrize === "door 1") {
+                        if (selectedPrize === "door 1") {
 
-                        showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
+                            showPrize[0].value = "You won a CAR";      //Alternate approach: showPrize[0].innerHTML = "You won a CAR";
 
-                    } else if (selectedPrize === "door 2") {
+                        } else if (selectedPrize === "door 2") {
 
-                        showPrize[0].value = "You won a CAT";
+                            showPrize[0].value = "You won a CAT";
 
-                    } else if (selectedPrize === "door 3") {
+                        } else if (selectedPrize === "door 3") {
 
-                        showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
+                            showPrize[0].value = "Sorry you won NOTHING, Thanks for playing, Goodbye!";
 
-                    } else if (selectedPrize === "door 4") {
+                        } else if (selectedPrize === "door 4") {
 
-                        showPrize[0].value = "You won a BOAT";
+                            showPrize[0].value = "You won a BOAT";
 
+                        } else {
+
+                            showPrize[0].value = "Sorry I did not understand your selection";
+                        }
                     } else {
-
-                        showPrize[0].value = "Sorry I did not understand your selection";
+                        showPrize[0].innerHTML = "Please make a selection and click submit";
                     }
-                } else {
-                    showPrize[0].innerHTML = "Please make a selection and click submit";
+                }
+            };
+        },
+
+        reset = function () {
+
+            document.getElementById("resetBtn").onclick = function () {
+                for (i = 0; i < prizes.length; i += 1) {
+
+                    if (prizes[i].checked) {
+                        prizes[i].checked = false;
+                    }
                 }
 
-            }
+                for (i = 0; i < showPrize.length; i += 1) {
 
+                    if (showPrize[i].value) {
+                        showPrize[i].value = "";
+                    } else {
+                        showPrize[0].innerHTML = "Please make a selection and click submit";
+                    }
+                }
+            };
         };
-    };
-
-    var reset = function () {
-
-        document.getElementById("resetBtn").onclick = function () {
-            for (var i = 0; i < prizes.length; i++) {
-
-                if (prizes[i].checked) {
-                    prizes[i].checked = false;
-                }
-            }
-
-            for (i = 0; i < showPrize.length; i++) {
-
-                if (showPrize[i].value) {
-                    showPrize[i].value = "";
-                } else {
-                    showPrize[0].innerHTML = "Please make a selection and click submit";
-                }
-            }
-        };
-    };
 
 
     window.onload = function () {
@@ -71,7 +70,6 @@
         reset();
 
     };
-
 
 }());
 
